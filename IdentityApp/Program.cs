43bb964e8466821266cfb16cc707931061be3783
vsprojects87 <vs.scala.619@gmail.com>
2024-04-26 08:23:43 +0530

    Add project files.

---
+++ IdentityApp/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using IdentityApp.Areas.Identity.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AppDBContextConnection") ?? throw new InvalidOperationException("Connection string 'AppDBContextConnection' not found.");
+
+builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDBContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
---
+++ IdentityApp/Readme.txt
@@ -0,0 +1,22 @@
+﻿Steps to create Identity project
+
+1. ASP.NET CORE MVC
+
+2. Add New Scaffolded item -> identity -> click on three dots(...) -> select _layout in shared folder as layout
+	for identity app
+
+3. choose functionality we want login,logout,register
+
+4. give name of dbcontext class (userdefined) AppDBContext
+
+5. give name of user class (userdefined) ApplicationUser
+
+6. add page link in _layout , we have partial view already generated named _LoginPartial we will add that
+                <partial name="_LoginPartial" />
+
+7. we can see register and login on web page after running project but we cant access them since they are razor
+	pages so we will need to add support for razor pages
+
+8. To do that we will go to program.cs -> before app.run() we will add middleware 'app.MapRazorPages();'
+
+
